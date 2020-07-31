f94a8e61daf178e03724a904ab8edd1213d51b3e
unknown <dossantosmarialudimila@gmail.com>
2020-07-31 14:53:32 -0300

    Mudança teste do views.py

--- aplicativo/views.py
+++ aplicativo/views.py
@@ -100,25 +100,3 @@ class PessoaRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 
 
 
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# #essas são os tipos de requisição http, que o apiview trabalja com ele
-# #em sequencia, ler os dados, alterar os dados, e deletar os dados
-# def pessoa_detail_change_and_delete(request, pk):
-#     try:
-#         pessoa = Pessoa.objects.get(pk = pk)
-#     except Pessoa.DoesNotExist:
-#         return Response(status = status.HTTP_404_NOT_FOUND)
-        
-#     if request.method == 'GET' :
-#         serializer = PessoaSerializer(pessoa)
-#         return Response(serializer.data)
-#     elif  request.method == 'PUT':
-#         serializer = PessoaSerializer(pessoa, data = request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-#     elif request.method == 'DELETE' :
-#         pessoa.delete()
-#         return Response(status = status.HTTP_204_NOT_CONTENT)
